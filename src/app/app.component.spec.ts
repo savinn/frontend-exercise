fb1a32a02e303c4dbc7526101c229a788cd1de1b
Polina Avramenko <polina@Polinas-MacBook-Pro.local>
2017-11-08 13:22:50 +0100

    tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,21 @@
 import { TestBed, async } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { SearchPipe } from "./search.pipe";
+import { Http } from "@angular/http";
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [FormsModule],
       declarations: [
-        AppComponent
+        AppComponent,
+        SearchPipe
       ],
+      providers: [
+        { provide: Http }
+      ]
     }).compileComponents();
   }));
 
@@ -17,16 +25,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
-  }));
 });
