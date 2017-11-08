4891a7ab39b2de3b3549d4cef5ae8ff375636346
Polina Avramenko <polina@Polinas-MacBook-Pro.local>
2017-11-08 17:23:53 +0100

    lint

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,18 +3,18 @@ import { FormsModule } from '@angular/forms';
 import { SearchPipe } from './search.pipe';
 import { AppService } from './app.service';
 import { Item } from './item.model';
-import { Observable } from "rxjs/Rx";
+import { Observable } from 'rxjs/Rx';
 
 import { AppComponent } from './app.component';
 
 let fixture: ComponentFixture<AppComponent>;
 let component: AppComponent;
 
-let appMock = {
+const appMock = {
   getItems(url): Observable<{}> {
     return Observable.of({});
   }
-}
+};
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -40,27 +40,26 @@ describe('AppComponent', () => {
     expect(component).toBeDefined();
   });
 
-  it('After clicking on item expects to set item.isChecked to true,to push the item into selectedItems array,to store the item in localStorage and to add class \'selection__checkbox-title--checked\'', () => {
-   
+  it('After clicking on item expects to set item.isChecked to true', () => {
     component.updateCheckedOptions(component.items[0]);
     fixture.whenStable().then(() => {
       fixture.detectChanges();
       expect(component.items[0].isChecked).toBe(true);
       expect(localStorage).not.toBeUndefined();
       expect(component.selectedItems.length).not.toEqual(0);
-      expect(fixture.nativeElement.querySelector(".selection__checkbox-title--checked")).not.toBeNull();
+      expect(fixture.nativeElement.querySelector('.selection__checkbox-title--checked')).not.toBeNull();
     });
   });
 
-  it('boo', () => {
+  it('After clicking on submit expects to clear all', () => {
     component.submitSelection();
     fixture.whenStable().then(() => {
-    fixture.detectChanges();
-    expect(component.items[0].isChecked).not.toBe(true);
-    expect(localStorage).toBeUndefined();
-    expect(component.selectedItems.length).toEqual(0);
-    expect(fixture.nativeElement.querySelector(".selection__checkbox-title--checked")).toBeNull();
-  });
+      fixture.detectChanges();
+      expect(component.items[0].isChecked).not.toBe(true);
+      expect(localStorage).toBeUndefined();
+      expect(component.selectedItems.length).toEqual(0);
+      expect(fixture.nativeElement.querySelector('.selection__checkbox-title--checked')).toBeNull();
+    });
   });
 
 });
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -14,6 +14,6 @@ export class AppService {
             .map(res => {
                 return res.json();
             }
-            )
+            );
     }
 }
